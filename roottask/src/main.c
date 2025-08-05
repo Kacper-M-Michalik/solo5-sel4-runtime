30cec77b3ee47c9de89502e349ae3180c7d0900e
Kacper Michalik <km924@cam.ac.uk>
2025-08-06 00:22:17 +0100

    Added test CMakes

---
+++ roottask/src/main.c
@@ -0,0 +1,16 @@
+#include <sel4/sel4.h>
+#include <sel4runtime.h>
+#include <simple/simple.h>
+#include <stdio.h>
+
+int main(void)
+{
+    seL4_BootInfo* bootinfo = sel4runtime_bootinfo();
+    simple_t simple_env;
+
+    simple_default_init_bootinfo(&simple_env, bootinfo);
+
+    printf("Booted and read BootInfo.");
+
+    return 0;
+}
