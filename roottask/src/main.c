0d1f378901817a6c947e5e7df1ca1d5078d96306
Kacper Michalik <km924@cam.ac.uk>
2025-08-06 22:52:36 +0100

    CMake cleanup, test main.

--- roottask/src/main.c
+++ roottask/src/main.c
@@ -2,16 +2,22 @@
 #include <sel4runtime.h>
 #include <simple/simple.h>
 #include <simple-default/simple-default.h>
+#include <sel4platsupport/bootinfo.h>
 #include <stdio.h>
 
-int main(void)
+int main(int argc, char *argv[]) 
 {
-    seL4_BootInfo* bootinfo = sel4runtime_bootinfo();
-    simple_t simple_env;
+    printf("Hello, World!\n");
 
-    simple_default_init_bootinfo(&simple_env, bootinfo);
+    seL4_BootInfo *info = platsupport_get_bootinfo();
 
-    printf("Booted and read BootInfo.");
+    size_t initial_cnode_object_size = 1lu<<(info->initThreadCNodeSizeBits);
+    printf("Initial CNode is %zu slots in size\n", initial_cnode_object_size);
+
+    simple_t simple_env;  
+    simple_default_init_bootinfo(&simple_env, info);
+
+    printf("Test\n");
 
     return 0;
 }
